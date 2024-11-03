ea456824c5114a39374ba7c355f77d884a62f04e
iftakher99 <19tonmoy99@gmail.com>
2024-11-03 09:56:25 +0600

    modified vercel.json

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 
 const getCredentials = (req, res) => {
-    const Credentials = { "username": "admin", "password": "123" }
-    res.status(200).json(Credentials)
-}
+  const Credentials = { username: 'admin', password: '1234' };
+  res.status(200).json(Credentials);
+};
 
-module.exports = { getCredentials }
+module.exports = { getCredentials };
